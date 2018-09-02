d073e0bd59bd17cf8496ac206d16f73907f72107
anujeet98 <anujeet98@gmail.com>
2018-09-02 20:28:31 +0530

    -m "validation "

---
+++ js/pro.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+$(function(){
+    $("form[name='reg']").validate({
+      rules:{
+          username:"required",
+          email:{
+                 required:true,
+                 email:true
+          },
+          password:{
+                 required:true,
+                 minlength:5
+          },
+          password2:{
+                 required:true,
+                 minlength:5,
+          }
+         }, 
+       messages:{
+              username:"please enter username",
+              email:"please enter email",
+              password:{
+                    required:"please enter paswd",
+                    minlength:"minimum 5 char"
+                   },
+              password2:{
+                    required:"please enter retype paswd",
+                    minlength:"minimum 5 char"
+                   }
+            
+             },      
+              submitHandler:function(form){
+                            form.submit();      
+                        }    
+          
+    });
+  });
+});
