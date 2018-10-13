39afe14c7965240d929a69783854197eb18e8217
anujeet98 <anujeet98@gmail.com>
2018-10-13 21:01:30 +0530

    search and find books in home.php ,sample database folder and some changes to css

---
+++ home.php
@@ -0,0 +1,263 @@
+<!doctype html>
+<html>
+<head>
+    
+<meta charset="utf-8"> 
+
+    
+    
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+</script>    
+<script>function toggleSidebar(){
+    document.getElementById('sidebar').classList.toggle('active');
+}
+</script>    
+    
+    
+    
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/owl.theme.default.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+    
+    
+<title>Dashboard page</title>    
+</head>
+    
+    
+    
+<body class="homebody">
+ 
+    <br>
+    
+    
+    <div id="container11"> 
+            <div id="sidebar">
+            <div class="toggle-btn" onclick="toggleSidebar()">
+                <img src="image/side.png">
+            </div>          
+            <ul>
+               <li>HOME</li>
+               <li>ABOUT</li>
+               <li>CONTACT</li>
+            </ul>  
+            </div>
+        
+
+           <div class="name"><h2>BOOK FACTORY</h2>
+           </div> 
+
+        
+         <div id="search-box">
+           <input type="text" name="search-box" id="search-box" placeholder=" Type a search " >
+
+          <button type="submit" id="sea_btn"> <i class="fa fa-search"  aria-hidden="true"></i></button>
+         </div> 
+    </div>
+    
+
+    
+    
+
+    <div class="container mt-3" style="padding-top: 70px">
+       <div class="row">
+           <div class="owl-carousel owl-theme">
+            <div class="item">
+               <div class="card">
+                  <img src="image/AM.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 1</h3>
+                      <h5>$<span class="text-center">23</span></h5>
+                      <p>Applied maths.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/AP.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 2</h3>
+                      <h5>$<span class="text-center">21</span></h5>
+                      <p>Applied physics.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/AC.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 3</h3>
+                      <h5>$<span class="text-center">10</span></h5>
+                      <p>Applied chemistry.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/DLDA.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 4</h3>
+                      <h5>$<span class="text-center">15</span></h5>
+                      <p>Digital Logic.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/DS.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 5</h3>
+                      <h5>$<span class="text-center">6</span></h5>
+                      <p>Data Structures.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/html.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 6</h3>
+                      <h5>$<span class="text-center">20</span></h5>
+                      <p>HTML5 and CSS3.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/CN.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 7</h3>
+                      <h5>$<span class="text-center">19</span></h5>
+                      <p>Computer Networks.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/eccf.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 8</h3>
+                      <h5>$<span class="text-center">11</span></h5>
+                      <p>Electronics circuits.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+               <div class="item">
+               <div class="card">
+                  <img src="image/dbms.jpg" alt="img" class="card-img-top">
+                  <div class="card-body">
+                      <h3>book 9</h3>
+                      <h5>$<span class="text-center">14</span></h5>
+                      <p>Database system.</p>
+                      <button class="btn btn-primary btn-sm">buy now</button>
+                    </div>  
+                   </div>   
+               </div>
+               
+             
+           </div>
+        
+        </div>
+    
+    </div>
+         
+    
+    
+    
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+    
+<script src="js/owl.carousel.js"></script>   
+
+<script>$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+        
+    },
+            items:4,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+
+</script>
+
+    
+  
+ 
+    <div>
+    <div id='container12'>
+    	<?php 
+   $connect=mysqli_connect("localhost","root","root","product");
+   $exp11=$_POST['search-box'];
+   echo exp11;
+   $sql="select * from prod where type='math';";
+   $result=mysqli_query($connect,$sql);
+   if ($connect) {
+  echo 'conected';
+} else {
+  echo 'not conected';
+}
+ ?>
+    	 <?php  
+             $output='';
+       	     if(mysqli_num_rows($result)>0)
+       	     {
+       	     	while($row=mysqli_fetch_array($result))
+       	     	{
+       	     		$output .='
+       	     		  <div class="col-md-4">
+                      <div style="border: 1px solid #ccc;padding: 12px; margin-bottom: 16px;height:375px;" align="text-center">
+                <img src=" data:image/jpeg;base64,'.base64_encode($row["image"]).' " class="img-res">
+                <h3>name:' .$row["name"].'</h3>
+                <h4>price:' .$row["price"].'</h4>
+              </div>
+       	     	</div>';
+       	     	}
+       	     } 
+       	     else
+       	     {
+       	     	$output="no product found";
+       	     }
+       	     echo $output;
+                
+                
+       	   ?>
+   </div>
+    </div>    
+<footer>
+    
+</footer>       
+</body>    
+</html>
